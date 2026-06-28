c5ea63c04fd7681bed33df112e3b2739b1c20288
= <mppozzobon8@gmail.com>
2026-06-27 23:27:27 -0300

    pipe

--- playwright.config.ts
+++ playwright.config.ts
@@ -77,6 +77,8 @@ export default defineConfig({
       url: 'http://localhost:3000',
       reuseExistingServer: !process.env.CI,
       timeout: 120_000,
+      stdout: 'pipe',  // <-- isso
+      stderr: 'pipe',
     },
   ],
 });
